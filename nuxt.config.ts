c59411c2f9b233ab0b5a17e000cc7d6c74c2a882
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-11-28 12:36:06 +0900

    clear

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,18 +7,14 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   // Env variables - https://nuxt.com/docs/getting-started/configuration#environment-variables-and-private-tokens
-  runtimeConfig: {
-    public: {
-      // Can be overridden by NUXT_PUBLIC_HELLO_TEXT environment variable
-      helloText: "Hello from the Edge 👋",
-    },
-  },
+  runtimeConfig: {},
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
   compatibilityDate: "2025-03-01",
 
   // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {},
+  css: ["~/assets/css/main.css"],
 
   // Development config
   eslint: {
