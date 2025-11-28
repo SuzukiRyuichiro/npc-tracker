a78045a4aacdb99e8b97a0654a61f825a6814231
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-11-28 15:45:45 +0900

    ws

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // https://nuxt.com/modules
-  modules: ["@nuxthub/core", "@nuxt/eslint", "@nuxt/ui", "nuxt-mapbox"],
+  modules: ["@nuxthub/core", "@nuxt/eslint", "@nuxt/ui", "nuxt-mapbox", "@vueuse/nuxt"],
 
   // https://devtools.nuxt.com
   devtools: { enabled: true },
@@ -13,7 +13,15 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-03-01",
 
   // https://hub.nuxt.com/docs/getting-started/installation#options
-  hub: {},
+  hub: {
+    blob: true,
+    workers: true,
+  },
+  nitro: {
+    experimental: {
+      websocket: true,
+    },
+  },
   css: ["~/assets/css/main.css"],
   mapbox: {
     accessToken: process.env.MAPBOX_API_KEY!,
